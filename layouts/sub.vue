952ab7f6c7713865b1de25c384f2550584914309
sora-77 <70363072+sora-77@users.noreply.github.com>
2025-11-10 11:59:09 +0900

    sub 레이아웃 생성

---
+++ layouts/sub.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- /NavigationHeader /> -->
+
+    <main class="sub">
+      <slot ></slot>
+    </main>
+
+    <NavigationFooter />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
