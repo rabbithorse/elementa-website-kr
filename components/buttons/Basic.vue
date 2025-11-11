6444145e88fe54c9d83ac0ea28bdff32c808d4ab
이다혜 <dh199606@naver.com>
2025-11-11 21:13:32 +0900

    Join us 작업

--- components/buttons/Basic.vue
+++ components/buttons/Basic.vue
@@ -2,74 +2,168 @@
   <button
     v-if="!href"
     class="btn"
-    :class="[sizeClass, colorClass]"
     @click="$emit('click')"
+    :class="[colorClass]"
   >
-    <slot />
+    <span class="inner" :class="[sizeClass]">
+      <slot />
+    </span>
+    <span class="hover_shadow"></span>
+    <span class="hover_shadow2"></span>
   </button>
 
   <a
     v-else
     class="btn"
-    :class="[sizeClass, colorClass]"
     :href="href"
     :target="target"
     :rel="target === '_blank' ? 'noopener noreferrer' : undefined"
+    :class="[colorClass]"
   >
-    <slot />
+
+    <span class="inner" :class="[sizeClass]">
+      <slot />
+    </span>
+    <span class="hover_shadow"></span>
+    <span class="hover_shadow2"></span>
   </a>
 </template>
 
 <style scoped>
-.btn {
-  background: linear-gradient(78deg, rgba(255,255,255,0.20) -4.53%, rgba(255,255,255,0.25) 113.59%);
-  box-shadow: -1.5px -1.5px 1.5px 0 rgba(255,255,255,0.20) inset,
-              2px 2px 2px 0 rgba(255,255,255,0.40) inset;
-  backdrop-filter: blur(10px);
-  color:#fff;
-  transition: all 0.3s ease;
-  clip-path: polygon(100% 0,100% 59%,87% 100%,0 100%,0 0);
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  white-space: nowrap;
-  position: relative;
-}
+  .btn {position:relative; display: inline-block;}
 
-.btn::before {
-  content: '';
-  position: absolute;
-  right: 12px;
-  bottom: -5px;
-  width: 2px;
-  height: 40px;
-  background: rgba(255,255,255,0.2);
-  transform: rotate(39deg);
-}
+  .btn .inner {
+    background: linear-gradient(78deg, rgba(255,255,255,0.20) -4.53%, rgba(255,255,255,0.25) 113.59%);
+    box-shadow: -1.5px -1.5px 1.5px 0 rgba(255,255,255,0.20) inset,
+                2px 2px 2px 0 rgba(255,255,255,0.40) inset;
+    backdrop-filter: blur(10px);
+    color:#fff;
+    transition: all 0.3s ease;
+    clip-path: polygon(100% 0,100% 33px, calc(100% - 20px) 100%,0 100%,0 0);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    white-space: nowrap;
+    position: relative;
+    z-index: 2;
+  }
 
-.btn.yellow {
-  background: linear-gradient(78deg, rgba(250,250,68,0.20), rgba(255,174,104,0.25));
-}
-.btn.blue {
-  background: linear-gradient(78deg, rgba(0,200,235,0.20), rgba(115,75,215,0.25));
-}
+  .btn .inner::before {
+    content: '';
+    position: absolute;
+    right: 4px;
+    bottom: -5px;
+    width: 2px;
+    height: 38px;
+    background: rgba(255,255,255,0.2);
+    transform: rotate(48deg);
+  }
 
-/* hover에 :deep() 추가 */
-.btn:hover {
-  box-shadow:
-    -10px -5px 20px rgba(143,104,255,0.20),
-    1.5px 1.5px 1.5px rgba(255,255,255,0.40) inset,
-    -1.5px -1.5px 1.5px rgba(0,0,0,0.40) inset,
-    0 0 20px rgba(0,0,0,0.70) inset,
-    10px 10px 20px rgba(68,223,250,0.20),
-    5px 5px 10px rgba(68,223,250,0.20),
-    1px 1px 5px rgba(68,223,250,0.40),
-    0 4px 4px rgba(0,0,0,0.25),
-    0 0 10px rgba(68,223,250,0.20);
-}
-.btn:hover::before {
-  display:none;
-}
+  .btn .lg.inner {
+    clip-path: polygon(100% 0,100% 35px, calc(100% - 20px) 100%,0 100%,0 0);
+  }
+
+  .btn .lg.inner::before {
+    right: 9px;
+    transform: rotate(38deg);
+  }
+
+  .btn .sm.inner {
+    clip-path: polygon(100% 0,100% 28px, calc(100% - 16px) 100%,0 100%,0 0);
+  }
+
+  .btn .sm.inner::before {
+    right: 0;
+    top: 9px;
+    transform: rotate(53deg);
+  }
+
+  .btn.yellow .inner {
+    background: linear-gradient(78deg, rgba(250,250,68,0.20), rgba(255,174,104,0.25));
+  }
+  .btn.blue .inner {
+    background: linear-gradient(78deg, rgba(0,200,235,0.20), rgba(115,75,215,0.25));
+  }
+
+  .btn .inner::after {content:''; display: block; width:100%; height: 100%; background: #fff;}
+
+  .btn:hover .inner {
+    box-shadow:
+      -10px -5px 20px rgba(143,104,255,0.20),
+      1.5px 1.5px 1.5px rgba(255,255,255,0.40) inset,
+      -1.5px -1.5px 1.5px rgba(0,0,0,0.40) inset,
+      0 0 20px rgba(0,0,0,0.70) inset,
+      10px 10px 20px rgba(68,223,250,0.20),
+      5px 5px 10px rgba(68,223,250,0.20),
+      1px 1px 5px rgba(68,223,250,0.40),
+      0 4px 4px rgba(0,0,0,0.25),
+      0 0 10px rgba(68,223,250,0.20);
+  }
+  .btn:hover .inner::before {
+    display:none;
+  }
+
+  .btn .hover_shadow {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: opacity 0.3s ease;
+    pointer-events: none;
+    z-index: 1;
+    opacity: 0;
+  }
+  .btn .hover_shadow::before {
+    content: '';
+    display: block;
+    width: calc(100% - 20px);
+    height: 4px;
+    background: #5aadbc;
+    position: absolute;
+    left: 0px;
+    bottom: -3px;
+    filter: blur(2.5px);
+    box-shadow: 2px 1px 0px 0px #239db4;
+  }
+
+  .btn .hover_shadow::after {
+    content: '';
+    display: block;
+    width: 2px;
+    height: calc(100% - 17px);
+    background: #5aadbc;
+    position: absolute;
+    right: 0;
+    top: 0;
+    filter: blur(2.5px);
+    box-shadow: 2px 1px 0px 0px #239db4;
+  }
+
+  .btn:hover .hover_shadow {
+    opacity: 1;
+  }
+
+  .btn .hover_shadow2 {
+    content: '';
+    display: block;
+    width: 4px;
+    height: 29px;
+    background: #5aadbc;
+    position: absolute;
+    right: 8px;
+    bottom: -7px;
+    filter: blur(2.5px);
+    box-shadow: 1px 1px 0px 0px #239db4;
+    transform: rotate(52deg);
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+
+  .btn:hover .hover_shadow2 {
+    opacity: 1;
+  }
 </style>
 
 <script setup>
--- pages/join_us.vue
+++ pages/join_us.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- top -->
-    <section class="subSectionTop pt-[130px]" ref="subSectionTop">
+    <section class="subSection pt-[130px]" ref="subSection">
       <div class="path-visual" ref="pathVisual">
         <div class="inner">
           <div class="big shape" ref="shapeBig"></div>
@@ -9,7 +9,7 @@
         </div>
       </div>
       <Container>
-        <h2 class="text-[11rem] leading-1 font-bold text-white subTitle">
+        <h2 class="text-[11rem] leading-1 font-bold text-white subPageTitle">
           <TypographyPrimary>
             J
           </TypographyPrimary>
@@ -36,90 +36,104 @@
           <TypographySecondary>게임 출시, 업데이트, 파트너십, 사회적 활동 등 다양한 소식을 투명하고 <br>생생하게 전달하며, 플레이어와 세계를 하나로 연결합니다.</TypographySecondary>
         </p>
 
-          <div>
-          <ButtonsBasic size="sm">Button Text</ButtonsBasic>
-          <ButtonsBasic @click="sayHi">Button Text</ButtonsBasic>
+        <div class="div" style="padding:50px;">
+          <ButtonsBasic size="sm" color="yellow">Button Text</ButtonsBasic>
+          <ButtonsBasic @click="sayHi" color="blue">Button Text</ButtonsBasic>
           <ButtonsBasic target="_blank" href="https://google.com" size="lg">Button Text</ButtonsBasic>
-          </div>
-        <div class="filter-box text-white mt-[5.5rem] py-[3.13rem] px-[7.5rem] flex gap-10 items-base">
+          <ButtonsBasic href="https://google.com">바로가기</ButtonsBasic>
+        </div>
+
+        <div class="filter-box text-white mt-[5.5rem] py-[3.13rem] px-[7.5rem] flex gap-10 items-end">
+          <div class="filter-glass"></div>
+          <div class="filter-shine"></div>
           <div class="filter-inner flex flex-col gap-[0.94rem]">
             <label>직군</label>
             <select>
               <option>전체</option>
-              <option>카테고리1</option>
-              <option>카테고리2</option>
+              <option>직군1</option>
+              <option>직군2</option>
             </select>
+            <div class="custom-select">
+              <div class="selected">전체</div>
+            </div>
           </div>
           <div class="filter-inner flex flex-col gap-[0.94rem]">
             <label>고용형태</label>
             <select>
               <option>전체</option>
-              <option>카테고리1</option>
-              <option>카테고리2</option>
+              <option>고용형태1</option>
+              <option>고용형태2</option>
             </select>
+            <div class="custom-select">
+              <div class="selected">전체</div>
+            </div>
           </div>
           <div class="filter-inner flex flex-col gap-[0.94rem]">
             <label>채용상태</label>
             <select>
               <option>전체</option>
-              <option>카테고리1</option>
-              <option>카테고리2</option>
+              <option>채용상태1</option>
+              <option>채용상태2</option>
             </select>
+            <div class="custom-select">
+              <div class="selected">전체</div>
+            </div>
           </div>
-          <input type="text" placeholder="검색어를 입력해주세요.">
-          <ButtonsBasic>검색</ButtonsBasic>
+          <div class="flex gap-[10px] grow sch-box">
+            <input type="text" placeholder="검색어를 입력해주세요." class="input-basic">
+            <button class="sch-btn" type="button" role="search"><i class="sch-icon"></i></button>
+          </div>
         </div>
       </Container>
 
       <!-- 채용 리스트 -->
-      <div class="list-recruit">
-        <p class="list-header">100개의 검색결과</p>
-        <ul>
-          <li>
-            <a href="">
-              <figure>
-                <img src="" alt="대표 이미지">
-              </figure>
-              <div class="text-box">
-                <h4>개발 PM 담당</h4>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <figure>
-                <img src="" alt="대표 이미지">
-              </figure>
-              <div class="text-box">
-                <h4>개발 PM 담당</h4>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <figure>
-                <img src="" alt="대표 이미지">
-              </figure>
-              <div class="text-box">
-                <h4>개발 PM 담당</h4>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <figure>
-                <img src="" alt="대표 이미지">
-              </figure>
-              <div class="text-box">
-                <h4>개발 PM 담당</h4>
-              </div>
-            </a>
-          </li>
-        </ul>
+      <div class="list-recruit pt-[100px] pb-[190px]" ref="listRecruit">
+        <div class="list-bg" ref="listBg"></div>
+        <Container>
+          <p class="text-2xl list-header pb-[2.5rem]">100개의 검색결과</p>
+          <ul class="grid grid-cols-4 gap-x-[1.25rem] gap-y-[5.63rem] list">
+            <li v-for="(n, index) in 12" :key="index">
+              <EffectCardHover>
+                <a class="cont" href="">
+                  <figure>
+                    <img src="~/assets/images/sub/join-us-img01.png" alt="대표 이미지">
+                  </figure>
+                  <div class="text-box">
+                    <div class="info">
+                      <div class="tit">
+                        <p class="d-day text-[0.69rem]">D-10</p>
+                        <h4 class="text-white text-xl">개발 PM 담당</h4>
+                        <p class="text-white text-[0.88rem]">경력직 ㆍ 경력 2-4년</p>
+                      </div>
+                      <p class="text-white date text-[0.88rem]">25.10.01~25.10.30</p>
+                    </div>
+                    <div class="apply">
+                      <span class="text-xs">100명의 지원자가 보고갔어요!</span>
+                      <ButtonsBasic size="sm" target="_blank">지원하러 가기</ButtonsBasic>
+                    </div>
+                  </div>
+                </a>
+                <div class="end-bg" :class="{ on: index === 7 }">
+                  <div class="inner-circle">
+                    <p class="text-[2.5rem] text-white font-bold">채용 마감</p>
+                  </div>
+                </div>
+              </EffectCardHover>
+            </li>
+          </ul>
+        </Container>
       </div>
       <!-- 채용 리스트 -->
     </section>
     <!-- top -->
+
+    <svg style="display: none">
+      <filter id="lg-dist" x="0%" y="0%" width="100%" height="100%">
+        <feTurbulence type="fractalNoise" baseFrequency="0.008 0.008" numOctaves="2" seed="92" result="noise" />
+        <feGaussianBlur in="noise" stdDeviation="2" result="blurred" />
+        <feDisplacementMap in="SourceGraphic" in2="blurred" scale="70" xChannelSelector="R" yChannelSelector="G" />
+      </filter>
+    </svg>
 
         
   </div>
@@ -140,43 +154,57 @@
   const shapeSmall = ref(null)
   const pathVisual = ref(null)
 
+  // 채용 리스트 배경 고정
+  const listRecruit = ref(null);
+  const listBg = ref(null);
+
   onMounted(() => {
     // Visual Parallax Effect
     if (process.client) {
-    // 큰 도형
-    $gsap.to(shapeBig.value, {
-      backgroundPositionY: '-120px',
-      ease: 'none',
-      scrollTrigger: {
-        trigger: shapeBig.value,
-        start: 'top top',
-        end: 'center top',
-        scrub: true, // 스크롤 위치에 따라 부드럽게 반응
-        //markers: true
-      },
-    })
+      // 큰 도형
+      $gsap.to(shapeBig.value, {
+        backgroundPositionY: '-100px',
+        ease: 'none',
+        scrollTrigger: {
+          trigger: shapeBig.value,
+          start: 'top top',
+          end: 'center top',
+          scrub: true,
+          // markers: true
+        },
+      })
 
-    // 작은 도형
-    $gsap.to(shapeSmall.value, {
-      backgroundPositionY: '-80px',
-      ease: 'none',
-      scrollTrigger: {
-        trigger: shapeSmall.value,
-        start: '10% 20%',
-        end: '50%% top',
-        scrub: true,
-        //markers: true
-      },
-    })
-  }
+      // 작은 도형
+      $gsap.to(shapeSmall.value, {
+        backgroundPositionY: '-80px',
+        ease: 'none',
+        scrollTrigger: {
+          trigger: shapeSmall.value,
+          start: '10% 20%',
+          end: '50%% top',
+          scrub: true,
+          //markers: true
+        },
+      })
+
+      $gsap.to(listRecruit.value, {
+        scrollTrigger: {
+          trigger: listRecruit.value,
+          start: 'top+=175 top',
+          end: 'bottom-=190 bottom',
+          scrub: 2,
+          //markers: true,
+          pin: listBg.value,
+          pinspacing: false
+        },
+      })
+    }
 
     // Custom Select
     document.querySelectorAll('.filter-inner select').forEach(select => {
 
-      const wrap = document.createElement('div');
-      wrap.className = 'custom-select';
-
-      const selected = document.createElement('div');
+      const wrap = select.nextElementSibling;
+      const selected = wrap.querySelector('.selected');
       selected.className = 'selected';
       selected.textContent = select.options[select.selectedIndex].text;
 
@@ -210,52 +238,17 @@
 </script>
 
 <style scoped>
-/* main title */
-.subTitle {
-  font-family: "Oswald";
-  line-height: 1.3;
-  display: flex;
-  gap: 12px;
-  transform: translateX(15px);
-}
-
-/* sub top */
-.subSectionTop {position: relative; overflow: hidden;}
-.subSectionTop .container {z-index: 3; position: relative;}
-.path-visual {position: absolute; right: 100px; top: 0; z-index: 1;  transition: all .5s;}
-.path-visual .inner {position: relative}
-
-/* selectbox */
-.filter-box {border:2px solid #fff; background: rgba(0, 0, 0, 0.15);
-box-shadow: -3px -3px 3px 0 rgba(255, 255, 255, 0.05) inset, 3px 3px 3px 0 rgba(255, 255, 255, 0.30) inset;
-filter: drop-shadow(20px 20px 20px rgba(0, 0, 0, 0.10)); border:1px solid rgba(255,255,255,0.15); backdrop-filter: blur(10px); position: relative; width: 100%;}
-.filter-box::before {content:''; display:block; width:73px; height:220px; background: url(~/assets/images/sub/reflection.svg) no-repeat center / cover; position: absolute;  left:0; top:0;}
-.filter-box .filter-inner select {display: none;}
-
-:deep(.filter-box .filter-inner .custom-select) {border-radius: 5px; background: rgba(255, 255, 255, 0.10);
-/* input box */
-box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0.25) inset, 0 0 4px 0 rgba(255, 255, 255, 0.60) inset, 0 4px 4px 0 rgba(0, 0, 0, 0.25); width:280px; position:relative;}
-:deep(.filter-box .filter-inner .custom-select .selected) {padding: 15px 25px; cursor:pointer; position:relative;}
-:deep(.filter-box .filter-inner .custom-select.active .selected::after) {content:''; display:block; width:100%; height:1px; background: #555; position:absolute; left:0; bottom:0;}
-:deep(.filter-box .filter-inner .custom-select ul) {padding: 10px 0; display: none; position:absolute; left:0; border-radius: 0  0 5px 5px; background: rgba(255, 255, 255, 0.10);
-/* input box */
-box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0) inset, 0 0 4px 0 rgba(255, 255, 255, 0.60) inset, 0 4px 4px 0 rgba(0, 0, 0, 0); width:100%;}
-:deep(.filter-box .filter-inner .custom-select.active){border-radius:5px 5px 0 0;}
-:deep(.filter-box .filter-inner .custom-select.active ul) {display: block;}
-:deep(.filter-box .filter-inner .custom-select ul li) {cursor:pointer; padding: 8px 15px;}
-:deep(.filter-box .filter-inner .custom-select ul li:hover) {background:rgba(0,0,0,0.15)}
-
-.hide-select {display: none;}
-
-.shape {clip-path: polygon(36.6% 0, 100% 0%, 63.6% 100%, 0 100%); width: 896px; height: 1080px; transform: scale(1);}
-.shape::before {content:''; display: block; width: 332px; height: 100%; background: url(~/assets/images/sub/line.svg) no-repeat center / cover; position: absolute; left: 0; top: 0;}
-.shape::after {content:''; display: block; width: 332px; height: 100%; background: url(~/assets/images/sub/line.svg) no-repeat center / cover; position: absolute; right: 0; top: 0;}
-.shape img {object-fit: cover; object-position: center; width:100%; height:100%;}
-.shape.big {background:url(~/assets/images/sub/sub_bg01.png) no-repeat center / cover;}
-.shape.small {background:url(~/assets/images/sub/sub_bg02.png) no-repeat top / cover;}
-.shape.small {position: absolute; right: calc(-100% + 398px); top: 300px;}
-.shape.small::before {top: 30px; left: -10px;}
-.shape.small::after {width:404px; height:6px; background:url(~/assets/images/sub/line_vertical.svg) no-repeat center / cover; top: 0; right: 210px;}
-
-.list-recruit {background: #05060B; z-index: 2; position: relative;}
+  .filter-glass {filter: url(#lg-dist); backdrop-filter: blur(12px); width: 100%; height:100%; position: absolute; left: 0;  bottom: 0; z-index: 1; background: url(~/assets/images/sub/otis-redding.png) repeat;}
+  .filter-shine {box-shadow: -3px -3px 3px 0 rgba(255, 255, 255, 0.05) inset, 3px 3px 3px 0 rgba(255, 255, 255, 0.30) inset; filter: drop-shadow(20px 20px 20px rgba(0, 0, 0, 0.10)); position: absolute; width: 100%; z-index: 1; height: 100%; left: 0; bottom: 0;}
+  :deep(.filter-box .filter-inner .custom-select) {border-radius: 5px; background: rgba(255, 255, 255, 0.10);
+  box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0.25) inset, 0 0 4px 0 rgba(255, 255, 255, 0.60) inset, 0 4px 4px 0 rgba(0, 0, 0, 0.25); width:280px; position:relative; border: 1px solid rgba(255, 255, 255, 0.2); height: 64px;}
+  :deep(.filter-box .filter-inner .custom-select .selected) {padding: 15px 25px; cursor:pointer; position:relative; height:100%; display:flex; align-items: center;}
+  :deep(.filter-box .filter-inner .custom-select .selected)::before {content:''; display:block; width:14px; height:14px; background:url(~/assets/images/sub/select-arrow.svg) no-repeat center / cover; position:absolute; right:20px; top:50%; transform: translateY(-50%);}
+  :deep(.filter-box .filter-inner .custom-select.active .selected::after) {content:''; display:block; width:100%; height:1px; background: #555; position:absolute; left:0; bottom:0;}
+  :deep(.filter-box .filter-inner .custom-select ul) {padding: 10px 0; display: none; position:absolute; left:0; border-radius: 0  0 5px 5px; background: rgba(255, 255, 255, 0.10);
+  box-shadow: 10px 10px 10px 0 rgba(0, 0, 0, 0) inset, 0 0 4px 0 rgba(255, 255, 255, 0.60) inset, 0 4px 4px 0 rgba(0, 0, 0, 0); width:100%;}
+  :deep(.filter-box .filter-inner .custom-select.active){border-radius:5px 5px 0 0;}
+  :deep(.filter-box .filter-inner .custom-select.active ul) {display: block;}
+  :deep(.filter-box .filter-inner .custom-select ul li) {cursor:pointer; padding: 8px 15px;}
+  :deep(.filter-box .filter-inner .custom-select ul li:hover) {background:rgba(0,0,0,0.15)}
 </style>
